2f3a4feb5738da31f2234881e36e8d78fbbd6d9a
Google AI Edge <noreply@google.com>
2024-05-08 18:02:00 -0700

    No public description
    PiperOrigin-RevId: 631983390

--- google3/third_party/odml/model_customization/quantization/examples/mnist_toy_model.py
+++ google3/third_party/odml/model_customization/quantization/examples/mnist_toy_model.py
@@ -1,70 +1,135 @@
-"""sample e2e run for the mnist toy model."""
+"""Sample end-to-end run for the MNIST toy model.
+
+This script quantizes the float mnist toy model and runs inference on a sample
+mnist image.
+
+blaze run
+//third_party/odml/model_customization/quantization/examples:mnist_toy_model
+--float_model_path=/tmp/conv_fc_mnist.tflite --img_path=/tmp/sample6.png
+"""
+
+import os
 
 from absl import app
-from google3.pyglib import gfile
-from google3.third_party.odml.model_customization.quantization import model_modifier
-from google3.third_party.odml.model_customization.quantization import model_validator
-from google3.third_party.odml.model_customization.quantization import params_generator
-from google3.third_party.odml.model_customization.quantization import recipe_manager
+from absl import flags
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+
+from google3.third_party.odml.model_customization.quantization import quantization_toolkit
 from google3.third_party.odml.model_customization.quantization import typing as qtyping
 from google3.third_party.odml.model_customization.quantization.utils import test_utils
 from google3.third_party.odml.model_customization.quantization.utils import tfl_interpreter_utils
-from google3.third_party.odml.model_customization.quantization.utils import validation_utils
+
+_OpExecutionMode = qtyping.OpExecutionMode
+_OpName = qtyping.TFLOperationName
+_TensorQuantConfig = qtyping.TensorQuantizationConfig
+_OpQuantConfig = qtyping.OpQuantizationConfig
+
+_FLOAT_MODEL_PATH = flags.DEFINE_string(
+    'float_model_path',
+    test_utils.get_path_to_datafile('../test_models/conv_fc_mnist.tflite'),
+    'The trained float MNIST toy model path.',
+)
+_IMG_PATH = flags.DEFINE_string(
+    'img_path',
+    test_utils.get_path_to_datafile('mnist_samples/sample6.png'),
+    'Path for the MNIST image to be predicted.',
+)
+_SAVE_PATH = flags.DEFINE_string(
+    'save_path',
+    '/tmp/',
+    'Path to save the quantized model and recipe.',
+)
 
 
-# TODO: b/336642947 - use the entry point API to generate the quantized .tflite.
+def read_img(img_path: str):
+  """Read MNIST image and normalize it.
+
+  Args:
+      img_path: Path to the image to be predicted.
+
+  Returns:
+      The normalized image.
+  """
+  image = Image.open(img_path)
+  data = np.asarray(image, dtype=np.float32)
+  if data.shape not in [(28, 28), (28, 28, 1)]:
+    raise ValueError(
+        'Invalid input image shape (MNIST image should have shape 28*28 or'
+        ' 28*28*1)'
+    )
+  # Normalize the image if necessary.
+  if data.max() > 1:
+    data = data / 255.0
+  return data.reshape((1, 28, 28, 1))
+
+
+def quantize(
+    float_model_path: str,
+    execution_mode: _OpExecutionMode = _OpExecutionMode.WEIGHT_ONLY,
+) -> quantization_toolkit.QuantizationResult:
+  """Quantize the float model.
+
+  Args:
+      float_model_path: Path to the float model.
+      execution_mode: Execution mode for the quantized model.
+
+  Returns:
+      QuantResult: quantization result
+  """
+  qt = quantization_toolkit.QuantToolkit(float_model_path)
+  qt.update_quantization_recipe(
+      regex='.*',
+      operation_name=_OpName.FULLY_CONNECTED,
+      op_config=_OpQuantConfig(
+          weight_tensor_config=_TensorQuantConfig(
+              num_bits=8,
+              symmetric=False,
+              channel_wise=True,
+          ),
+          execution_mode=execution_mode,
+      ),
+  )
+  return qt.quantize()
+
+
+def inference(quantized_tflite: bytes, image_path: str) -> np.ndarray:
+  """Run inference on the quantized model.
+
+  Args:
+      quantized_tflite: Quantized model in bytes.
+      image_path: Path to the image to be predicted.
+
+  Returns:
+      Predicted category probabilities for the image.
+  """
+  tflite_interpreter = tf.lite.Interpreter(model_content=quantized_tflite)
+  tflite_interpreter.allocate_tensors()
+  data = read_img(image_path)
+  tfl_interpreter_utils.invoke_interpreter_once(tflite_interpreter, [data])
+  tflite_output_details = tflite_interpreter.get_output_details()[0]
+  result = tflite_interpreter.get_tensor(tflite_output_details['index'])
+  return np.squeeze(result)
+
+
 def main(_) -> None:
-  model_path = test_utils.get_path_to_datafile(
-      '../test_models/conv_fc_mnist.tflite'
+  if not os.path.exists(_FLOAT_MODEL_PATH.value):
+    raise ValueError(
+        'Model file does not exist. Please check the .tflite model path.'
+    )
+  if not os.path.exists(_IMG_PATH.value):
+    raise ValueError('Image file does not exist. Please check the image path.')
+  quant_result = quantize(_FLOAT_MODEL_PATH.value, _OpExecutionMode.WEIGHT_ONLY)
+  category_probabilities = inference(
+      quant_result.quantized_model, _IMG_PATH.value
   )
-  recipe_manager_instance = recipe_manager.RecipeManager()
-  params_generator_instance = params_generator.ParamsGenerator(model_path)
-  model_modifier_instance = model_modifier.ModelModifier(model_path)
-  global_recipe = [
-      {
-          'regex': '.*',
-          'operation': 'FULLY_CONNECTED',
-          'algorithm_key': 'ptq',
-          'op_config': {
-              'weight_tensor_config': {
-                  'dtype': qtyping.TensorDataType.INT,
-                  'num_bits': 8,
-                  'symmetric': False,
-                  'channel_wise': True,
-              },
-              'execution_mode': qtyping.OpExecutionMode.WEIGHT_ONLY,
-          },
-          'override_algorithm': True,
-      },
-  ]
-  recipe_manager_instance.load_quantization_recipe(global_recipe)
-  tensor_quantization_params = (
-      params_generator_instance.generate_quantization_parameters(
-          recipe_manager_instance
-      )
+  predicted_category = np.argmax(category_probabilities)
+  print(
+      f'Model predicts the image as {predicted_category} with probability'
+      f' {category_probabilities[predicted_category]}',
   )
-  new_model_binary = model_modifier_instance.modify_model(
-      tensor_quantization_params
-  )
-  save_model_path = test_utils.get_path_to_datafile('/tmp/test.tflite')
-  with gfile.GFile(save_model_path, 'wb') as output_file_handle:
-    output_file_handle.write(new_model_binary)
-
-  reference_model_interpreter = tfl_interpreter_utils.create_tfl_interpreter(
-      model_path
-  )
-  dataset = test_utils.create_random_normal_dataset(
-      reference_model_interpreter.get_input_details(), 3, 666
-  )
-  result = model_validator.compare_model(
-      model_path,
-      save_model_path,
-      dataset,
-      False,
-      validation_utils.mean_squared_difference,
-  )
-
-  print(result)
+  quant_result.save(_SAVE_PATH.value, model_name='mnist_toy_model')
 
 
 if __name__ == '__main__':
