2de738abc7be95574933245708445afd79591209
Pauline Sho <psho@google.com>
2025-05-13 17:00:15 -0700

    Add hadamard rotation tests with golden inputs.
    PiperOrigin-RevId: 758431575

--- ai_edge_quantizer/algorithms/uniform_quantize/hadamard_rotation_test.py
+++ ai_edge_quantizer/algorithms/uniform_quantize/hadamard_rotation_test.py
@@ -119,6 +119,55 @@ class HadamardRotationFullyConnectedTest(parameterized.TestCase):
     if qparams.hadamard is not None:
       self.assertEqual(qparams.hadamard.hadamard_size, 32)
 
+  def test_get_tensor_quant_params_golden_1(self):
+    test_data = np.ones((6, 6))
+    # expected:
+    #   [[127   0 127   0 127   0]
+    #    [127   0 127   0 127   0]
+    #    [127   0 127   0 127   0]
+    #    [127   0 127   0 127   0]
+    #    [127   0 127   0 127   0]
+    #    [127   0 127   0 127   0]]
+    expected = np.tile([127, 0], [6, 3])
+    qparams = hadamard_rotation.get_tensor_quant_params(
+        self._op_info,
+        self._op_info.op_quant_config.weight_tensor_config,
+        test_data,
+        self._tensor_name_to_qsv,
+    )
+    self.assertIsNotNone(qparams.quantized_data)
+    np.testing.assert_array_equal(
+        np.array(qparams.quantized_data), expected
+    )
+
+  def test_get_tensor_quant_params_golden_2(self):
+    # test_data:
+    #   [[1 2 1 2 1 2]
+    #    [3 4 3 4 3 4]
+    #    [1 2 1 2 1 2]
+    #    [3 4 3 4 3 4]
+    #    [1 2 1 2 1 2]
+    #    [3 4 3 4 3 4]]
+    test_data = np.tile([[1, 2], [3, 4]], [3, 3])
+    # expected:
+    #   [[127 -42 127 -42 127 -42]
+    #    [127 -18 127 -18 127 -18]
+    #    [127 -42 127 -42 127 -42]
+    #    [127 -18 127 -18 127 -18]
+    #    [127 -42 127 -42 127 -42]
+    #    [127 -18 127 -18 127 -18]]
+    expected = np.tile([[127, -42], [127, -18]], [3, 3])
+    qparams = hadamard_rotation.get_tensor_quant_params(
+        self._op_info,
+        self._op_info.op_quant_config.weight_tensor_config,
+        test_data,
+        self._tensor_name_to_qsv,
+    )
+    self.assertIsNotNone(qparams.quantized_data)
+    np.testing.assert_array_equal(
+        np.array(qparams.quantized_data), expected
+    )
+
   def test_raise_missing_tensor_content(self):
     with self.assertRaisesWithPredicateMatch(
         ValueError, lambda err: "weight tensor" in str(err)
